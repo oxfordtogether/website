677318345de1e9213ae6d4d32143c90f2a8a650d
vickyclose <vicky.r.close@gmail.com>
2020-03-16 19:52:26 +0000

    Move header components into helper file

---
+++ src/components/helpers/header.js
@@ -0,0 +1,153 @@
+import styled from "styled-components"
+import { Link } from "gatsby"
+import Img from "gatsby-image"
+import { OutboundLink } from "gatsby-plugin-google-analytics"
+
+export const HeaderWrapper = styled.header`
+  background-color: #f8f8f8;
+  padding: 160px 0 80px 0;
+  position: relative;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 5vw));
+  @media (max-width: ${props => props.theme.screen.md}) {
+    padding: 80px 0 40px 0;
+  }
+`
+
+export const Subtitle = styled.h5`
+  font-size: 16px;
+  color: ${props => props.theme.color.accent};
+  letter-spacing: 0px;
+  margin-bottom: 16px;
+`
+
+export const HeaderTextGroup = styled.div`
+  margin: 0;
+
+  h1 {
+    margin: 0 0 24px;
+    color: ${props => props.theme.color.primary};
+  }
+
+  h2 {
+    margin-bottom: 24px;
+    ${props => props.theme.font_size.regular}
+  }
+
+  p {
+    margin-bottom: 48px;
+  }
+`
+
+export const Flex = styled.div`
+  display: grid;
+  justify-content: space-between;
+  align-content: center;
+  grid-template-columns: 1fr 1fr;
+  @media (max-width: ${props => props.theme.screen.md}) {
+    grid-template-columns: 1fr;
+    grid-gap: 64px;
+  }
+`
+
+export const HeaderForm = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-bottom: 16px;
+  margin-bottom: 16px;
+
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    flex-direction: column;
+  }
+`
+
+export const FormSubtitle = styled.span`
+  ${props => props.theme.font_size.xxsmall}
+`
+
+export const FormSubtitleLink = styled(Link)`
+  color: ${props => props.theme.color.secondary};
+  padding-bottom: 1px;
+  margin-left: 8px;
+  text-decoration: none;
+  border-bottom: 1px solid ${props => props.theme.color.secondary};
+`
+
+export const ButtonLink = styled(OutboundLink)`
+  text-decoration: none;
+`
+
+export const HeaderInput = styled.input`
+  font-weight: 500;
+  font-size: 16px;
+  color: ${props => props.theme.color.primary};
+  line-height: 42px;
+  width: 100%;
+  text-align: left;
+  height: 60px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${props => props.theme.color.secondary};
+  border-image: initial;
+  border-radius: 4px;
+  padding: 8px 16px;
+  outline: 0px;
+  &:focus {
+    box-shadow: inset ${props => props.theme.color.secondary} 0px 0px 0px 2px;
+  }
+  @media (max-width: ${props => props.theme.screen.md}) {
+    margin-bottom: 8px;
+  }
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    display: block;
+    width: 100%;
+  }
+`
+
+export const HeaderButton = styled.button`
+  font-weight: 500;
+  font-size: 14px;
+  color: white;
+  letter-spacing: 1px;
+  height: 60px;
+  display: block;
+  margin-left: 8px;
+  margin-bottom: 8px;
+  text-transform: uppercase;
+  text-decoration: none;
+  cursor: pointer;
+  white-space: nowrap;
+  background: ${props => props.theme.color.secondary};
+  border-radius: 4px;
+  padding: 0px 40px;
+  border-width: 0px;
+  border-style: initial;
+  border-color: initial;
+  border-image: initial;
+  outline: 0px;
+  &:hover {
+    box-shadow: rgba(110, 120, 152, 0.22) 0px 2px 10px 0px;
+  }
+  @media (max-width: ${props => props.theme.screen.md}) {
+  }
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    margin-left: 0;
+  }
+`
+export const ImageWrapper = styled.div`
+  justify-self: end;
+  align-self: center;
+  @media (max-width: ${props => props.theme.screen.md}) {
+    justify-self: center;
+  }
+`
+
+export const StyledImage = styled(Img)`
+  width: 500px;
+  @media (max-width: ${props => props.theme.screen.md}) {
+    width: 400px;
+  }
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    width: 300px;
+    display: none;
+  }
+`
