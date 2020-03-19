7a71e421b60d038c71df1d8bc1077a2526fb8c4c
vickyclose <vicky.r.close@gmail.com>
2020-03-19 21:08:31 +0000

    Add navbar and css

--- src/pages/neighbourhoods-map.js
+++ src/pages/neighbourhoods-map.js
@@ -1,26 +1,42 @@
 import React from "react"
 import styled from "styled-components"
 import Layout from "../components/common/layout/layout"
+import SEO from "../components/common/layout/seo"
+import Navigation from "../components/common/navigation/navigation"
+import { Section } from "../components/global"
+import Footer from "../components/sections/footer"
 
 const MapContainer = styled.div`
   margin: 20px;
+  iframe {
+    width: 75%;
+    @media screen and (max-width: 800px) {
+      width: 100%;
+    }
+    display: block;
+    margin: auto;
+  }
 `
 MapContainer.displayName = "MapContainer"
 
 const NeighbourhoodsMap = () => (
   <Layout>
-    <MapContainer>
-      <iframe
-        src="https://www.google.com/maps/d/u/0/embed?mid=1uXcwsMZa4GC9UuzXhlJiTWsvUDlG2nN3"
-        width="800"
-        height="800"
-        frameborder="0"
-        style={{ border: "0" }}
-        allowfullscreen=""
-        aria-hidden="false"
-        tabindex="0"
-      ></iframe>
-    </MapContainer>
+    <SEO title="Neighbourhoods map" />
+    <Navigation />
+    <Section>
+      <MapContainer>
+        <iframe
+          src="https://www.google.com/maps/d/u/0/embed?mid=1uXcwsMZa4GC9UuzXhlJiTWsvUDlG2nN3"
+          height="800"
+          frameborder="0"
+          style={{ border: "0" }}
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+      </MapContainer>
+    </Section>
+    <Footer />
   </Layout>
 )
 
--- src/pages/postcode-lookup.js
+++ src/pages/postcode-lookup.js
@@ -4,7 +4,7 @@ import Layout from "../components/common/layout/layout"
 import SEO from "../components/common/layout/seo"
 import Navigation from "../components/common/navigation/navigation"
 import PostcodeLookup from "../components/sections/postcode-lookup"
-import { HeaderWrapper, Flex } from "../components/helpers/header"
+import { Flex } from "../components/helpers/header"
 import Footer from "../components/sections/footer"
 import { Container } from "../components/global"
 
