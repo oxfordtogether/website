7a227d14a2bfc02d3732fa408fd6e08035a4bfa0
Tom Close <tom.close@cantab.net>
2020-03-26 11:41:10 +0000

    Switch to new practical support form

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,22 @@
+export const mutualAidRequestForm =
+  "https://docs.google.com/forms/d/e/1FAIpQLSfN1r4HByqEoYgNStYTQH7-MP3P3yqylnid3hwic2CVk9YoKQ/viewform"
+export const mutualAidVolunteerForm =
+  "https://docs.google.com/forms/d/e/1FAIpQLSfN1r4HByqEoYgNStYTQH7-MP3P3yqylnid3hwic2CVk9YoKQ/viewform"
+export const mutualAidFacebook =
+  "https://www.facebook.com/groups/202604247629195"
 
-export const mutualAidRequestForm = "https://docs.google.com/forms/d/e/1FAIpQLSfN1r4HByqEoYgNStYTQH7-MP3P3yqylnid3hwic2CVk9YoKQ/viewform";
-export const mutualAidVolunteerForm = "https://docs.google.com/forms/d/e/1FAIpQLSfN1r4HByqEoYgNStYTQH7-MP3P3yqylnid3hwic2CVk9YoKQ/viewform";
-export const mutualAidFacebook = "https://www.facebook.com/groups/202604247629195";
+export const oxfordHubVolunteerForm =
+  "https://docs.google.com/forms/d/e/1FAIpQLScfYSgB7G-eWo6rLZga11g5-ictGxYRq1OvHyx270dZcCLwCQ/viewform"
 
-export const oxfordHubVolunteerForm = "https://docs.google.com/forms/d/e/1FAIpQLScfYSgB7G-eWo6rLZga11g5-ictGxYRq1OvHyx270dZcCLwCQ/viewform";
+export const newGithubIssue =
+  "https://github.com/tomclose/covid-support-oxford/issues/new"
 
-export const newGithubIssue = "https://github.com/tomclose/covid-support-oxford/issues/new";
+export const oxfordHubPhoneSupportForm =
+  "https://docs.google.com/forms/d/e/1FAIpQLScrnON6LlkJvxoB9O2Mq-OnK_Wp7iV0GW2JYUZ7hv_hSHrMbQ/viewform"
 
-export const oxfordHubPhoneSupportForm = "https://docs.google.com/forms/d/e/1FAIpQLScrnON6LlkJvxoB9O2Mq-OnK_Wp7iV0GW2JYUZ7hv_hSHrMbQ/viewform";
+export const oxfordHubCommunityPartnersForm =
+  "https://docs.google.com/forms/d/1crUT-En_d6e2ciKG3B1AEB3MiMD0u1mVQQFbU04eJ5U/viewform"
 
-export const oxfordHubCommunityPartnersForm = "https://docs.google.com/forms/d/1crUT-En_d6e2ciKG3B1AEB3MiMD0u1mVQQFbU04eJ5U/viewform";
-
-export const oxfordHubPracticalSupportForm = "https://docs.google.com/forms/d/e/1FAIpQLSdR_Rot_eDoRpWgWUSe8xDUEY6uNf-3rHoqURCQfYxC4Kztmg/viewform"
+export const oxfordHubPracticalSupportForm =
+  "https://docs.google.com/forms/d/e/1FAIpQLSfzyoIGvaTj25AuN6rhhjfG5PAw8wzZo_a6Wr4S_CrAAago_w/viewform"
+// export const oxfordHubPracticalSupportForm = "https://docs.google.com/forms/d/e/1FAIpQLSdR_Rot_eDoRpWgWUSe8xDUEY6uNf-3rHoqURCQfYxC4Kztmg/viewform"
