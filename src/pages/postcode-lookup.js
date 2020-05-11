75af85e4c0081bd252888141db71c843c1a7ddb6
vickyclose <vicky.r.close@gmail.com>
2020-05-11 21:58:34 +0100

    Remove navigation buttons from postcode lookup

--- src/pages/postcode-lookup.js
+++ src/pages/postcode-lookup.js
@@ -2,14 +2,12 @@ import React from "react"
 
 import Layout from "../components/common/layout/layout"
 import SEO from "../components/common/layout/seo"
-import Navigation from "../components/common/navigation/navigation"
 import PostcodeLookup from "../components/sections/postcode-lookup"
 import Footer from "../components/sections/footer"
 
 const PostcodeLookupPage = () => (
   <Layout>
-    <SEO title="Covid Support" />
-    <Navigation />
+    <SEO title="Oxford Together - Postcode Lookup" />
     <PostcodeLookup />
     <Footer />
   </Layout>
